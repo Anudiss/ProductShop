374194e2abda907d5784ba2c85ddd7a9a22edfaa
Anudiss <liobara31@gmail.com>
2022-12-11 22:13:11 +0300

    lasldawdasdawd

--- ProductShop/ViewModels/NavigationVM.cs
+++ ProductShop/ViewModels/NavigationVM.cs
@@ -6,44 +6,44 @@ namespace ProductShop.ViewModels
 {
     public class NavigationVM : ViewModelBase
     {
-        private static NavigationVM _instance;
+        //private static NavigationVM _instance;
 
-        public static NavigationVM Instance =>
-            _instance ?? (_instance = new NavigationVM());
+        //public static NavigationVM Instance =>
+        //    _instance ?? (_instance = new NavigationVM());
 
-        private object _currentView;
+        //private object _currentView;
 
-        public object CurrentView
-        {
-            get => _currentView;
-            set
-            {
-                _currentView = value;
-                OnPropertyChanged();
-            }
-        }
+        //public object CurrentView
+        //{
+        //    get => _currentView;
+        //    set
+        //    {
+        //        _currentView = value;
+        //        OnPropertyChanged();
+        //    }
+        //}
 
-        #region Command
-        public RelayCommand Products { get; set; }
-        public RelayCommand Suppliers { get; set; }
-        public RelayCommand Orders { get; set; }
-        public RelayCommand Supplyes { get; set; }
-        public RelayCommand OpenAuth { get; set; }
-        #endregion
+        //#region Command
+        //public RelayCommand Products { get; set; }
+        //public RelayCommand Suppliers { get; set; }
+        //public RelayCommand Orders { get; set; }
+        //public RelayCommand Supplyes { get; set; }
+        //public RelayCommand OpenAuth { get; set; }
+        //#endregion
 
-        public NavigationVM()
-        {
-            Products = new RelayCommand((arg) => CurrentView = new ProductsPageVM());
-            Orders = new RelayCommand((arg) => CurrentView = new OrderPageVM());
-            Suppliers = new RelayCommand((arg) => CurrentView = new SupplierPageVM());
-            Supplyes = new RelayCommand((arg) => CurrentView = new SupplyPageVM());
-            OpenAuth = new RelayCommand((arg) =>
-            {
-                new AuthWindow().Show();
-                MainWindow.Instance.Close();
-            });
+        //public NavigationVM()
+        //{
+        //    Products = new RelayCommand((arg) => CurrentView = new ProductsPageVM());
+        //    Orders = new RelayCommand((arg) => CurrentView = new OrderPageVM());
+        //    Suppliers = new RelayCommand((arg) => CurrentView = new SupplierPageVM());
+        //    Supplyes = new RelayCommand((arg) => CurrentView = new SupplyPageVM());
+        //    OpenAuth = new RelayCommand((arg) =>
+        //    {
+        //        new AuthWindow().Show();
+        //        MainWindow.Instance.Close();
+        //    });
 
-            Products.Execute();
-        }
+        //    Products.Execute();
+        //}
     }
 }
